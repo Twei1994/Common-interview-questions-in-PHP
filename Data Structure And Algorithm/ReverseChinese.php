9c7ccc57bcbdcf37bb7d0285befb2d1ef4bb2cc1
Twei1994 <tju213@gmail.com>
2017-09-26 18:29:09 +0800

    rewrite reverseChinese.php

--- Data Structure And Algorithm/ReverseChinese.php
+++ Data Structure And Algorithm/ReverseChinese.php
@@ -1,1 +1,9 @@
 <?php
+function reverse($str){
+	$r = array();
+	for($i=0; $i<mb_strlen($str); $i++){
+		$r[] = mb_substr($str, $i, 1, 'UTF-8');
+	}
+	return implode(array_reverse($r));
+}
+echo reverse('www.phpha.com天涯PHP博客');
